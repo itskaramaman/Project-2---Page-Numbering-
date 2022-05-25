e4238a36d8a9d1af82e949e9c17bc3d5a87d9b68
Karamjeet Singh Jandu <singhkaramjeetamanapple@gmail.com>
2022-05-24 22:28:03 -0700

    pagination completed

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// users per page will be 10
+const usersPerPage = 10;
+
+// get user contacts
+const contactItems = document.getElementsByClassName('contact-item');
+// Get number of pages
+const numberOfPage = Math.ceil(contactItems.length / usersPerPage);
+
+// create html for pagination
+let paginationHTML = "";
+for(let i=1; i<=numberOfPage; i++) {
+    paginationHTML+=`<li><a href="javascript:void(0)" id="${i}" onclick="updatePage(${i})">${i}</a></li>`;
+}
+// set the inner html of pagination
+document.getElementById('pagination-list').innerHTML = paginationHTML;
+
+// load the first page 
+window.addEventListener('load', (event)=>{
+    updatePage(1);
+});
+
+// update the page as per pagination
+function updatePage(id) {
+    // get the start index and end index of users to display
+    const startIndex = id * usersPerPage - 10;
+    const endIndex = id * usersPerPage;
+
+    // removing the active class 
+    let activeLink = document.getElementsByClassName('active');
+    if(activeLink.length > 0) {
+        activeLink[0].classList.remove('active');
+    }
+
+    // adding the active clas to the pagination link clicked
+    document.getElementById(id)?.classList.add('active');
+
+    // set the list of users
+    for(let i=0; i<contactItems.length; i++) {
+        if(i<startIndex || i>=endIndex) {
+            contactItems[i].style.display = 'none';
+        } else {
+            contactItems[i].style.display = 'block';
+        }
+    }
+}
+
+
